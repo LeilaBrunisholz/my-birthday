2204ecb18eaecfdec869ff4d65fc5ffd04b617cc
Leila Brunisholz <lhb22920@gmail.com>
2024-01-17 14:59:35 -0500

    text and img changed in js

---
+++ script.js
@@ -0,0 +1,10 @@
+function year2024(){
+  
+        document.getElementById('imageChange').src = 'dogs.jpg'
+        document.getElementById('response-text').classList ='text-warning'
+        document.getElementById('inner-text').classList ='hi'
+
+
+
+}
+
