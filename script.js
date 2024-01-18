704e82e5ecd7562d06dd0659012cfbf326c8a1ed
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-01-18 16:53:38 +0000

    1868 done

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function year1955(){
         document.getElementById('textChange').innerText='Steve Jobs was also born on this day. Steve was the CEO of apple and was also born February 24, 1955.'
 }
 
+function year1868(){
+        document.getElementById('imageChange').src='andrew.jpg'
+        document.getElementById('textChange').innerText='Andrew Johnson was impeached on this day. Andrew was the 17th president and the first one to be impeached.'
 
+}
+
+my birthday is the day the worlds first opera premiered
 
 
 
