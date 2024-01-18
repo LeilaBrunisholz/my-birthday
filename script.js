426454a0a3e46b63ac7e910e92b4af1ca17f0eff
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-01-18 16:42:12 +0000

    steve done

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function year1955(){
-        document.getElementById('imageChange').src = 'dogs.jpg'
-        document.getElementById('textChange').innerText='ah'
+        document.getElementById('imageChange').src = 'steve.jpg'
+        document.getElementById('textChange').innerText='Steve Jobs was also born on this day. Steve was the CEO of apple and was also born February 24, 1955.'
 }
+
+
 
 
 
