a4d18eec2f63d39b9dbc6b2b3eb6f5d167e8271f
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-01-22 19:43:33 +0000

    added citations

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function year1955(){
         document.getElementById('imageChange').src = 'steve.jpg'
-        document.getElementById('textChange').innerText='Steve Jobs was also born on this day. Steve was the CEO of apple and was also born February 24, 1955.'
+        document.getElementById('textChange').innerText='Steve Jobs was also born on this day. Steve was the CEO of apple and was also born February 24, 1955. - Found on the website "Wikipedia.org"'
 }
 
 function year1868(){
         document.getElementById('imageChange').src='andrew.jpg'
-        document.getElementById('textChange').innerText='Andrew Johnson was impeached on this day. Andrew was the 17th president and the first one to be impeached.'
+        document.getElementById('textChange').innerText='Andrew Johnson was impeached on this day. Andrew was the 17th president and the first one to be impeached. - found on the website "senate.gov"'
 
 }
 
 function year1607(){
         document.getElementById('imageChange').src='opera.png'
-        document.getElementById('textChange').innerText='LOrfeo by Claudio Monteverdi was the first recognized opera and it premiered on February 24, 1607.'
+        document.getElementById('textChange').innerText='LOrfeo by Claudio Monteverdi was the first recognized opera and it premiered on February 24, 1607. - Found on the website "on this day"'
 
 }
 
 function year1981(){
         document.getElementById('imageChange').src='royal.jpeg'
-        document.getElementById('textChange').innerText='Prince Charles and Princess Diana announced their engagement at Buckingham Palace on February 24, 1981.'
+        document.getElementById('textChange').innerText='Prince Charles and Princess Diana announced their engagement at Buckingham Palace on February 24, 1981. - Found on the website "People.com"'
 
 }
 
 function year2002(){
         document.getElementById('imageChange').src='olympics.webp'
-        document.getElementById('textChange').innerText='On February 24, 2002, the winter olympic games in Salt Lake city ended. '
+        document.getElementById('textChange').innerText='On February 24, 2002, the winter olympic games in Salt Lake city ended. - Found on the website "britannica.com" '
 
 }
 
