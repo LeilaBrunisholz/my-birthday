65938ee71b50b7cc0e9cc8afb3283fe1ed0abd7b
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-01-26 19:08:49 +0000

    dates changed

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 function year1955(){
         document.getElementById('imageChange').src = 'steve.jpg'
         document.getElementById('textChange').innerText='Steve Jobs was also born on this day. Steve was the CEO of apple and was also born February 24, 1955. - Found on the website "Wikipedia.org"'
+        document.getElementById('textChanges').innerText='1955'
 }
 
 function year1868(){
         document.getElementById('imageChange').src='andrew.jpg'
         document.getElementById('textChange').innerText='Andrew Johnson was impeached on this day. Andrew was the 17th president and the first one to be impeached. - found on the website "senate.gov"'
+        document.getElementById('textChanges').innerText='1868'
 
 }
 
 function year1607(){
         document.getElementById('imageChange').src='opera.png'
         document.getElementById('textChange').innerText='LOrfeo by Claudio Monteverdi was the first recognized opera and it premiered on February 24, 1607. - Found on the website "on this day"'
+        document.getElementById('textChanges').innerText='1607'
 
 }
 
 function year1981(){
         document.getElementById('imageChange').src='royal.jpeg'
         document.getElementById('textChange').innerText='Prince Charles and Princess Diana announced their engagement at Buckingham Palace on February 24, 1981. - Found on the website "People.com"'
-
+        document.getElementById('textChanges').innerText='1981'
 }
 
 function year2002(){
         document.getElementById('imageChange').src='olympics.webp'
         document.getElementById('textChange').innerText='On February 24, 2002, the winter olympic games in Salt Lake city ended. - Found on the website "britannica.com" '
+        document.getElementById('textChanges').innerText='2002'
 
 }
-
 
 
 
